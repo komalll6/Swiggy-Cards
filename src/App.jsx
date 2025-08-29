ffe9b695c7eb8777632e5d004dd37277b77c6fb0
komal kalra <komalkalra698@gmail.com>
2025-08-29 20:16:06 +0530

    Updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ function App() {
         fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9628669&lng=77.57750899999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
         .then((response) => response.json())
         .then((data) => {
-            const restaurantsList = data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-
+            const restaurantsList = data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants || [];
             setRestaurants(restaurantsList);
         })
         .catch((err) => console.error('Fetch error:', err));
     }, []);
-
 return(
     <div>
         <h1>Restaurants in Jalandhar</h1>
     <div>
     {restaurants.map((restaurant) => (
-      <div>
-        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.info.cloudinaryImageId}`} alt={restaurant.info.name} />
+      <div key={restaurant.info.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
+        <img style={{ width: '100px', height: 'auto' }} src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.info.cloudinaryImageId}`} alt={restaurant.info.name} />
         <h1>{restaurant.info.name}</h1>
         <p>{restaurant.info.avgRating}</p>
         <p>{restaurant.info.cuisines.join(", ")}</p>
