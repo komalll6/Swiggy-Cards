bd3fe22c3c3689bd2d731fe8e2c00277a3594b83
komal kalra <komalkalra698@gmail.com>
2025-09-02 21:09:33 +0530

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9628669&lng=77.57750899999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
         .then((response) => response.json())
         .then((data) => {
-            const restaurantsList = data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants || [];
+            const restaurantsList = data.cards[0].card.card.gridElements.infoWithStyle.info || [];
             setRestaurants(restaurantsList);
         })
         .catch((err) => console.error('Fetch error:', err));
@@ -15,13 +15,18 @@ function App() {
 return(
     <div>
         <h1>Restaurants in Jalandhar</h1>
-    <div>
+    <div style={{display:"grid", gridTemplateColumns:"repeat(5, 1fr)", gap:"10px", padding:"10px"}}>
     {restaurants.map((restaurant) => (
-      <div key={restaurant.info.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
-        <img style={{ width: '100px', height: 'auto' }} src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.info.cloudinaryImageId}`} alt={restaurant.info.name} />
-        <h1>{restaurant.info.name}</h1>
-        <p>{restaurant.info.avgRating}</p>
-        <p>{restaurant.info.cuisines.join(", ")}</p>
+      <div key={info.id} style={{transition:"transform 0.2s", "&:hover":{transform:"scale(1.05)"},border:"1px solid #e2dfdfff", borderRadius:"5px", overflow:"hidden", boxShadow:"0 4px 8px rgba(0, 0, 0, 0.1)", hover:"shadow-lg", hoverTransform:"scale(1.05)"}}>
+        <div style={{cursor:"pointer", display:"flex", justifyContent:"center", alignItems:"center", marginTop:"5px",marginBottom:"-20px"}}>
+        <img style={{width:"230px", height:"200px", borderRadius:"8px",}} src={`https://media-assets.swiggy.com/swiggy/image/upload/${info.ImageId}`} alt={info.action.link} />
+        </div>
+        <div style={{textAlign:"left", padding:"20px", fontFamily:"Arial, sans-serif", marginTop:"-30px", marginBottom:"-30px", paddingBottom:"20px"}}>
+        <h3>{info.action.text}</h3>
+        <p style={{marginTop:"-15px", fontWeight:"bold"}}> <img src="https://cdn-icons-png.flaticon.com/128/3334/3334338.png" alt="" style={{width:"15px", height:"15px", marginRight:"2px", marginBottom:"-1px"}} />{info.action.accessibility.altText} • {info.action.accessibility.altTextCta}</p>
+        {/* <p style={{marginTop:"-15px", color:"gray"}}>{restaurant.info.sla.slaString}</p> */}
+        {/* <p style={{marginTop:"-15px", color:"gray"}}>{restaurant.info.cuisines.join(", ")}</p> */}
+        </div>
       </div>
      ))}
     </div>
